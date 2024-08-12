92d97e54256451fb6046b0100c9d9a84d1898af4
vhforbes <vhforbes@gmail.com>
2024-08-12 09:47:21 -0300

    feat: added ranionalizing to gdcRecur

--- iteration-recurssion/recursive-gdc.py
+++ iteration-recurssion/recursive-gdc.py
@@ -1,3 +1,10 @@
+# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+
+# If b = 0, then the answer is a (BASE CASE)
+
+# Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+
+
 def gdcRecur(a, b):
     if b == 0:
         print(a)
