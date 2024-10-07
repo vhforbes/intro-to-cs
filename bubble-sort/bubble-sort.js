49db8db0c5c662832afe3fd7b7421e4ccaeff7df
vhforbes <vhforbes@gmail.com>
2024-10-07 09:00:01 -0300

    practice makes perfect

---
+++ bubble-sort/bubble-sort.js
@@ -0,0 +1,23 @@
+const nums = [8, 2, 4, 1, 3];
+
+const bubbleSort = (nums) => {
+  let swapped = true;
+
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i + 1];
+        nums[i + 1] = nums[i];
+        nums[i] = temp;
+
+        swapped = true;
+      }
+    }
+  }
+
+  return nums;
+};
+
+console.log(bubbleSort(nums));
