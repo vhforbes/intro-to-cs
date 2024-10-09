3905059c62f9d41c6d6f9148f75af1a9fbc57aa8
vhforbes <vhforbes@gmail.com>
2024-10-09 07:37:34 -0300

    removing to solve tomorrow

--- recurssion/nested-arrays.js
+++ recurssion/nested-arrays.js
@@ -2,21 +2,7 @@ const nestedOne = [10, [12, 14, [1], [16, [20]]]]; // 94
 const nestedTwo = [1, [2], 3]; // 6
 
 const nestedAdd = (array) => {
-  let sum = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    let current = array[i];
-
-    // Base case, calls itself it needs to add more or just add the number
-    if (Array.isArray(current)) {
-      sum += nestedAdd(current);
-    } else {
-      sum += current;
-    }
-  }
-
-  console.log(sum);
-  return sum;
+  return;
 };
 
 nestedAdd(nestedOne);
