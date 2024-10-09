1f3a46ea4155a718fcb8ede29ec3c8899d02a969
vhforbes <vhforbes@gmail.com>
2024-10-09 07:06:44 -0300

    feat: solving insertion sort again

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -1,5 +1,18 @@
 const nums = [8, 2, 4, 1, 3];
 
-const insertionSort = (nums) => {};
+const insertionSort = (nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    let current = nums[i];
+
+    let j = i - 1;
+
+    for (j; nums[j] > current && j >= 0; j--) {
+      nums[j + 1] = nums[j];
+      nums[j] = current;
+    }
+  }
+
+  console.log(nums);
+};
 
 insertionSort(nums);
