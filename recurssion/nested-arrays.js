002fc44173d82f034e223152fc9ace1434e520cc
vhforbes <vhforbes@gmail.com>
2024-10-21 21:27:02 -0300

    feat: more...

--- recurssion/nested-arrays.js
+++ recurssion/nested-arrays.js
@@ -2,7 +2,20 @@ const nestedOne = [10, [12, 14, [1], [16, [20]]]]; // 94
 const nestedTwo = [1, [2], 3]; // 6
 
 const nestedAdd = (array) => {
-  return;
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      console.log("Its a array, calling nestedAdd for: ", array[i]);
+
+      sum += nestedAdd(array[i], sum);
+    } else {
+      console.log("NOT array, trying to add: ", array[i]);
+      sum += array[i];
+    }
+  }
+
+  return sum;
 };
 
-nestedAdd(nestedOne);
+console.log(nestedAdd(nestedOne));
