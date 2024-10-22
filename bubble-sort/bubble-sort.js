14800534a9b00d1c83df03f6f7f40ec76acedea8
vhforbes <vhforbes@gmail.com>
2024-10-21 21:04:40 -0300

    more...

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -8,16 +8,16 @@ const bubbleSort = (nums) => {
 
     for (let i = 0; i < nums.length; i++) {
       if (nums[i] > nums[i + 1]) {
-        let temp = nums[i + 1];
-        nums[i + 1] = nums[i];
-        nums[i] = temp;
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
 
         swapped = true;
       }
     }
   }
 
-  console.log(nums);
+  return nums;
 };
 
-bubbleSort(nums);
+console.log(bubbleSort(nums));
--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -1,13 +1,25 @@
 const nums = [8, 2, 4, 1, 3];
 
 const insertionSort = (nums) => {
+  // Consider the first part of your list as sorted
   for (let i = 1; i < nums.length; i++) {
     let current = nums[i];
+    console.log("Current: ", current);
 
+    // Look at the unsorted part of the list
     let j = i - 1;
 
+    console.log(nums[j]);
+
+    // This means the current index is smaller then the element that comes before it
+    // [8, 2] - 2 = current
+    //        - 8 = j - 1
+    // So we compare both and insert the current before if smaller
     for (j; nums[j] > current && j >= 0; j--) {
+      // Insert the bigger number one position ahead
       nums[j + 1] = nums[j];
+
+      // The smaller number in the position that we had the bigger one
       nums[j] = current;
     }
   }
